09bdbda44dbbbd54f3dee3b1bea2037f6c7e917f
tusharx0809 <tusharbhatia0809@outlook.com>
2026-07-10 17:00:44 +0530

    added username column in users table instead of profiles table

--- authentication-service/Postgres/functions/func_register_user.sql
+++ authentication-service/Postgres/functions/func_register_user.sql
@@ -1,15 +1,30 @@
-CREATE OR REPLACE FUNCTION func_register_user(in_f_name varchar(100), in_l_name varchar(100), in_email varchar(100), in_hashed_password varchar(255), in_username varchar(100), in_dob date)
-RETURNS VOID 
-AS 
-$$
+-- FUNCTION: public.func_register_user(character varying, character varying, character varying, character varying, character varying, date)
+
+-- DROP FUNCTION IF EXISTS public.func_register_user(character varying, character varying, character varying, character varying, character varying, date);
+
+CREATE OR REPLACE FUNCTION public.func_register_user(
+	in_f_name character varying,
+	in_l_name character varying,
+	in_email character varying,
+	in_hashed_password character varying,
+	in_username character varying,
+	in_dob date)
+    RETURNS void
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
 	DECLARE
 			new_user_id INT;
 	BEGIN
-		INSERT INTO users(email,hashed_password)
-		VALUES(in_email,in_hashed_password)
+		INSERT INTO users(email,username,hashed_password)
+		VALUES(in_email,in_username,in_hashed_password)
 		RETURNING user_id INTO new_user_id;
 
-		INSERT INTO profiles(user_id,f_name,l_name,user_name,date_of_birth)
-		VALUES(new_user_id,in_f_name,in_l_name,in_user_name,in_dob);
+		INSERT INTO profiles(user_id,f_name,l_name,date_of_birth)
+		VALUES(new_user_id,in_f_name,in_l_name,in_dob);
 	END;
-$$ LANGUAGE plpgsql;
+$BODY$;
+
+ALTER FUNCTION public.func_register_user(character varying, character varying, character varying, character varying, character varying, date)
+    OWNER TO postgres;
