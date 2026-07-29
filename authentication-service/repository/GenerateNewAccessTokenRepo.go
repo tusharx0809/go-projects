b3fa891ddb19be32e220d9e15b8e28532f5db86d
tusharx0809 <tusharbhatia0809@outlook.com>
2026-07-29 13:27:43 +0530

    added revoked logic in refresh token

--- authentication-service/repository/GenerateNewAccessTokenRepo.go
+++ authentication-service/repository/GenerateNewAccessTokenRepo.go
@@ -1,11 +1,24 @@
 package repository
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 func (r *AuthRepo) GetUserUID(userID int) (string, error) {
 	var userUID string
+
+	var isRevoked bool
+	checkQuery := "SELECT revoked FROM refresh_tokens WHERE user_id = $1"
+
+	err := r.Authdb.QueryRow(context.Background(), checkQuery, userID).Scan(&isRevoked)
+
+	if isRevoked {
+		return "", errors.New("Please login Again!")
+	}
+
 	query := "SELECT user_uid FROM users WHERE user_id = $1"
-	err := r.Authdb.QueryRow(context.Background(), query, userID).Scan(&userUID)
+	err = r.Authdb.QueryRow(context.Background(), query, userID).Scan(&userUID)
 
 	if err != nil {
 		return "", err
